50db8fd506daa555e9fc439c43d0b526823b3447
Moonyll <s.pouwel@laposte.net>
2018-12-17 17:00:44 +0100

    Ex7

---
+++ Ex7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valeur de BC ?");
+            int BC = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valeur de CA ?");
+            int CA = int.Parse(Console.ReadLine());
+            int a = (CA * CA) + (BC * BC);
+            double AC = Math.Sqrt(a);
+            Console.WriteLine("AB vaut : " +AC);
+        }
+    }
+}
