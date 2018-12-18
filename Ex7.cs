1ea1a02ac6e626090d4e93e9cc76f53fbd17863a
Moonyll <45941071+Moonyll@users.noreply.github.com>
2018-12-18 10:57:43 +0100

    Update Ex7.cs

--- Ex7.cs
+++ Ex7.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp17
             int BC = int.Parse(Console.ReadLine());
             Console.WriteLine("Valeur de CA ?");
             int CA = int.Parse(Console.ReadLine());
-            int a = (CA * CA) + (BC * BC);
+            int a = (CA * CA) + (BC * BC); // Math.Pow(BC,2) + Math.Pow(CA,2);
             double AC = Math.Sqrt(a);
             Console.WriteLine("AB vaut : " +AC);
         }
